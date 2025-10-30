69590a3ce47234c5244fb41da7a378b71c720159
Eric Aboudaram <eric.aboudaram-ext@pole-emploi.fr>
2025-10-30 15:43:45 +0100

    mise en place d une annotation unique

--- public/page2/page2.ts
+++ public/page2/page2.ts
@@ -1,8 +1,9 @@
-import { Template } from "../../src/lib/annotations/template";
-import { Controller } from "../../src/lib/annotations/controller";
+import {Template} from "../../src/lib/annotations/template";
+import {Controller} from "../../src/lib/annotations/controller";
 
 @Controller("Page2Controller")
 @Template({ html: "/page1/page1.html" })
+// @ControllerWithTemplate("Page2Controller", "/page1/page1.html")
 export class Page2Controller {
   init() {}
 }
---
+++ src/lib/annotations/controllerWithTemplate.ts
@@ -0,0 +1,21 @@
+import { Template } from "./template";
+import { Controller } from "./controller";
+
+/**
+ * Combinaison de @Controller et @Template pour garantir l'ordre correct.
+ * @param path - route du controller
+ * @param html - nom du fichier HTML associé
+ * @param css - nom du fichier css associé
+ */
+export function ControllerWithTemplate(
+  path: string,
+  html?: string,
+  css?: string,
+) {
+  return function (target: any) {
+    // ⚠️ Template doit être appliqué en premier
+    // Controller appliqué ensuite
+    Template({ html: html, css: css })(target);
+    Controller(path)(target);
+  };
+}
